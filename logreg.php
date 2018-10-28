fe83f7608394c47397736461a8ff8cd17919c536
alspok <alspok@gmail.com>
2018-10-28 22:34:45 +0200

    ...

---
+++ logreg.php
@@ -0,0 +1,55 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' href='logreg.css'>
+
+    </head>
+    <body>
+        <h2 style=' text-align: center; padding: 50px'>Maistas login arba registracija</h2>
+        <div class='container flex'>
+            <div class='log-box'>
+                <div style='text-align: center; padding: 2px'><h3>Login</h3></div>
+                    <div class='flex'>
+                        <form action='' name='logform' method='post'> 
+                            <div><b>Vartotojo vardas:</b><br><input type='text' name='logname' value=''></div>
+                            <br>
+                            <div><b>Slaptažodis:</b><br><input type='text' name='logpassword' value=''></div>
+                            <br>
+                            <div><input type='submit' name='logsubmit' value="Įvesti"></div>
+                            <?php if(isset($_POST['logsubmit'])) logInput($_POST); ?>
+                        </form>
+                    </div>
+            </div>
+            <div class='reg-box'>
+                <div style='text-align: center; padding: 2px'><h3>Registracija</h3></div>
+                <div class='flex'>
+                    <form action='' name='regform' method='post'>
+                        <div><b>Vartotojo vardas:</b><br><input type='text' name='regname' value=''></div>
+                        <br>
+                        <div><b>Slaptažodis:</b><br><input type='text' name='regpassword' value=''></div>
+                        <br>
+                        <div><b>El. pašto adresas:</b><br><input type='text' name='regmail' value=''></div>
+                        <br>
+                        <div><b>Telefonas:</b><br><input type='text' name='regphone' value=''></div>
+                        <br>
+                        <div><input type='submit' name='regsubmit' value="Įvesti"></div>
+                        <?php if(isset($_POST['regsubmit'])) regInput($_POST); ?>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<?php
+function logInput($logData){
+    var_dump($logData);
+}
+
+function regInput($regData){
+    if(isset($regData['regname']) && isset($regData['regpassword']) &&  isset($regData['regmail']) && isset($regData['regphone'])){
+            var_dump($_POST);
+    }
+    else{echo 'empty input';}
+}
