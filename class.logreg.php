11f2c3064422fb95cfdbd30c958c9aaf6efc5eae
alspok <alspok@gmail.com>
2018-10-30 15:07:45 +0200

    ...

--- class.logreg.php
+++ class.logreg.php
@@ -1,9 +1,40 @@
 <?php
 /* User registration and login class*/
 
-class LogReg{
+class MySqlConnection{
 
-    function login($userData){
+    function constructor($serverName, $userName, $userPassword, $dbName, $tblName){
+
+        $this->serverName = $serverName;
+        $this->userName = $userName;
+        $this->userPassword = $userPassword;
+        $this->dbName = $dbName;
+        $this->tblName = $tblName;
+        $this->dbConnection($this->serverName, $this->userName, $this->userPassword);
+    }
+    
+    function dbConnection(){
         
+        $conn = mysqli_connect($this->serverName, $this->userName, $this->userPassword)
+            or die('Could not connect to server.');
+        var_dump($conn);
+
+        return $conn;
+    }
+
+    function dbView(){
+
+    }
+
+    function dbInsert($dbQuery){
+
+    }
+
+    function dbDelete($dbQuery){
+
+    }
+
+    function dbUpdate($dbQuery){
+
     }
 }
