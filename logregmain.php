da647b0bf9c0cbb61e159e38c4249c599a19c5e2
alspok <alspok@gmail.com>
2018-11-04 00:36:28 +0200

    .

--- logregmain.php
+++ logregmain.php
@@ -1,20 +1,20 @@
 <?php
 
-require_once 'class.logreg.php';
-echo 'in logregmain';
-
 function logSubmit($logData){
 
-    var_dump($logData);
-
     if( !empty($logData['logname']) && !empty($logData['logpassword'])){
-            $conn = new MySqlConnection('localhost', 'root', '', 'db_maistas');
-            $table = $conn->tableView('tbl_logreg');
-            while($row = $table->fetch_assoc()){
-                var_dump($row);
-                echo $row['id'] . '<br>' . $row['username'] . '<br>' . $row['password'];
+        $conn = new MySqlConnection('localhost', 'root', '', 'db_maistas');
+        $table = $conn->tableView('tbl_logreg');
+        $boolen = false;
+        while($row = $table->fetch_assoc()){
+            // echo '<br>' . $row['id'] . '<br>' . $row['username'] . '<br>' . $row['password'];
+            if(($logData['logname'] == $row['username']) && ($logData['logpassword'] == $row['password'])){
+                echo "<p style='color: green'> Loged in!</p>";
+                $boolen = true;
+                break;
             }
-            echo '<p>OK</p>';
+        }
+        if(!$boolen) echo "<p style='color: red'>Entries does not mach. Try again,</p>";
     }
     else{
         echo "<p style='color: red'>Enter all tags</p>";
