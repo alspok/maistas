29b8f0e491763d1435c355d253e2135e64818fca
alspok <alspok@gmail.com>
2018-11-06 17:43:32 +0200

    .

--- maistasmain.php
+++ maistasmain.php
@@ -3,9 +3,9 @@
 function viewMaistas($tblName){
 
     $conn = new MySqliConnection('localhost', 'root', '', 'db-alspok');
-    $table = $conn->tableView('tbl_maistas');
+    $table = $conn->tableView($tblName);
     $i = 1;
     while($row = $table->fetch_assoc()){
-        echo '<tr><td>' . $i++ . '</td><td>' . $row['product'] . '</td><td>' . $row['quantity'] . '</td><td>' . $row['notes'] . '</td></tr>';
+        echo '<tr><td>' . $row['id'] . '</td><td>' . $i++ . '</td><td>' . $row['product'] . '</td><td>' . $row['quantity'] . '</td><td>' . $row['notes'] . '</td></tr>';
     }
 }                        
